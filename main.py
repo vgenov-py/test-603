25c4e97ad4c2b449e985a553248bb881246deace
CICE <genovese.work@protonmail.com>
2021-11-10 21:08:17 +0100

    se ha subido

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ print("Desde develop")
 print("desde develop 2")
 
 # adjuntado por Juan Carlos
-<<<<<<< HEAD
-# Adjuntado por Vito
-=======
 
+# Adjuntado por Vito
 print("aran")
->>>>>>> 3e4d8219435871d0a5ea1f2d65f2e459091035dd
+
 
 # Esto será agregado desde ops.sh
+# Esto será agregado desde ops.sh 2
