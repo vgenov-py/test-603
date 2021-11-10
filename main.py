abc6a6954f13688eb1eb8328a130b8caf306c46a
CICE <genovese.work@protonmail.com>
2021-11-10 21:59:01 +0100

    random

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-print("Esto irá a git")
-print("segundo commit")
+import random
 
-
-print("Desde develop")
-print("desde develop 2")
-
-# adjuntado por Juan Carlos
-
-# Adjuntado por Vito
-print("aran")
-
-
-# Esto será agregado desde ops.sh
-# Esto será agregado desde ops.sh 2
+a = [1,2,3,4]
+print(random.choice(a))
+print(random.random())
